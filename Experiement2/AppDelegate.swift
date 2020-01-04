dee7c8f5a20bfcd740e92d8ac6dcc998ff8359d4
izloop <peter@ptip.uk>
2020-01-04 14:21:46 +0100

    Initial Commit

---
+++ Experiement2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Experiement2
+//
+//  Created by Izloop on 1/4/20.
+//  Copyright © 2020 IPL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Experiement2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Experiement2
+//
+//  Created by Izloop on 1/4/20.
+//  Copyright © 2020 IPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
